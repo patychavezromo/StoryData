eabc2776b7af385072e73d8d0b6dfad90396527b
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2025-03-03 21:02:29 -0600

    feat: Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+
+import "./App.css";
+import Title from "./components/header";
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Title text="Studio Ghibli" />
+      <p>Hello world</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+function Title({text}) {
+  return (
+    <>
+      <h1>{text}</h1>
+    </>
+  );
+}
+
+export default Title;
